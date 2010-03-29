54b15756fa8ea0bfba8952113863b43cb00dc4d4
Renato Lucindo <lucindo@gmail.com>
2010-03-29 01:06:38 -0300

    defaults.cc

---
+++ single/defaults.cc
@@ -0,0 +1,6 @@
+// Copyright (c) 2010 R. Lucindo (lucindo@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or http://boost.org/LICENSE_1_0.txt)
+
+#include "defaults.hh"
