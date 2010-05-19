2a90ea3d243e65ab1aa70bd8fb408f5a44a063b2
Renato Lucindo <lucindo@gmail.com>
2010-05-19 10:36:00 -0300

    removed Boost.Asio support

--- single/keypers.cc
+++ single/keypers.cc
@@ -6,13 +6,6 @@
 #include "keypers.hh"
 #include "config.hh"
 #include "log.hh"
-#include "server.hh"
-
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
-
-#include <pthread.h>
-#include <signal.h>
 
 int main(int argc, char** argv) try
 {
@@ -30,33 +23,6 @@ int main(int argc, char** argv) try
     }
 
     l(lg::info, "will bing at: %s:%s [%d threads]", config.host().c_str(), config.port().c_str(), config.thread_pool_size());
-
-	// Block all signals for background thread.
-    sigset_t new_mask;
-    sigfillset(&new_mask);
-    sigset_t old_mask;
-    pthread_sigmask(SIG_BLOCK, &new_mask, &old_mask);	
-
-	// Run server in background thread.
-    keyper::server server(config.host(), config.port(), config.thread_pool_size());
-    boost::thread main_thread(boost::bind(&keyper::server::run, &server));
-
-	// Restore previous signals.
-    pthread_sigmask(SIG_SETMASK, &old_mask, 0);
-
-    // Wait for signal indicating time to shut down.
-    sigset_t wait_mask;
-    sigemptyset(&wait_mask);
-    sigaddset(&wait_mask, SIGINT);
-    sigaddset(&wait_mask, SIGQUIT);
-    sigaddset(&wait_mask, SIGTERM);
-    pthread_sigmask(SIG_BLOCK, &wait_mask, 0);
-    int sig = 0;
-    sigwait(&wait_mask, &sig);
-
-    // Stop the server.
-    server.stop();
-    main_thread.join();
 
     return 0;
 }
